8437ad2b372100402042ba8e0e3fdf01dbd403f7
Min Jiwon <peace.cheejecake@gmail.com>
2022-05-22 00:01:19 +0900

    style: fuzzy

--- src/components/Fuzzystring.tsx
+++ src/components/Fuzzystring.tsx
@@ -1,16 +1,14 @@
 import { escapeRegExp } from 'lodash';
 
-// 인덱스 시그니처
 interface ObjType {
   [ch: string]: number;
 }
 
 const ch2pattern = (ch: string) => {
   const offset = 44032; /* '가'의 코드 */
-  // 한국어 음절
+
   if (/[가-힣]/.test(ch)) {
     const chCode = ch.charCodeAt(0) - offset;
-    // 종성이 있으면 문자 그대로를 찾는다.
     if (chCode % 28 > 0) {
       return ch;
     }
@@ -18,7 +16,7 @@ const ch2pattern = (ch: string) => {
     const end = begin + 27;
     return `[\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
   }
-  // 한글 자음
+
   if (/[ㄱ-ㅎ]/.test(ch)) {
     const con2syl: ObjType = {
       ㄱ: '가'.charCodeAt(0),
@@ -36,8 +34,7 @@ const ch2pattern = (ch: string) => {
     const end = begin + 587;
     return `[${ch}\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
   }
-  // 그 외엔 그대로 내보냄
-  // escapeRegExp는 lodash에서 가져옴
+
   return escapeRegExp(ch);
 };
 
@@ -45,10 +42,9 @@ function FuzzyString(inputValue: string) {
   const pattern = inputValue
     .split('')
     .map(ch2pattern)
-    .map((prev) => {
-      return `(${prev})`;
-    })
+    .map((prev) => `(${prev})`)
     .join('.*?');
   return new RegExp(pattern, 'i');
 }
+
 export default FuzzyString;
--- src/hooks/useSearchAll.ts
+++ src/hooks/useSearchAll.ts
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useQuery } from 'react-query';
+import { uniqBy } from 'lodash';
 
+import FuzzyString from 'components/Fuzzystring';
 import { useAppSelector } from 'hooks';
 import { getAllDiseasesApi } from 'services/search';
 import { getSearchValue } from 'states/value/searchValue';
 import { incrementCount } from 'states/apiCount';
 import { setSearchResultList } from 'states/searchResultList';
-import FuzzyString from 'components/Fuzzystring';
-import { uniqBy } from 'lodash';
 
 export const useSearchAll = () => {
   const searchValue = useAppSelector(getSearchValue);
@@ -38,28 +38,24 @@ export const useSearchAll = () => {
     const regex = FuzzyString(searchValue);
     const diseasesData = uniqBy(data, 'sickCd');
     const resultData = diseasesData
-      .filter((row) => {
-        return regex.test(row.sickNm);
-      })
-      .map((row) => {
-        return {
-          sickCd: row.sickCd,
-          sickNm: row.sickNm.replace(regex, (match, ...groups) => {
-            const letters = groups.slice(0, groups.length - 2);
-            let lastIndex = 0;
-            const highlighted = [];
-            for (let i = 0, l = letters.length; i < l; i += 1) {
-              const idx = match.indexOf(letters[i], lastIndex);
-              highlighted.push(match.substring(lastIndex, idx));
-              highlighted.push(',');
-              highlighted.push(`|${letters[i]}|`);
-              highlighted.push(',');
-              lastIndex = idx + 1;
-            }
-            return highlighted.join('');
-          }),
-        };
-      });
+      .filter((row) => regex.test(row.sickNm))
+      .map((row) => ({
+        sickCd: row.sickCd,
+        sickNm: row.sickNm.replace(regex, (match, ...groups) => {
+          const letters = groups.slice(0, groups.length - 2);
+          let lastIndex = 0;
+          const highlighted = [];
+          for (let i = 0, l = letters.length; i < l; i += 1) {
+            const idx = match.indexOf(letters[i], lastIndex);
+            highlighted.push(match.substring(lastIndex, idx));
+            highlighted.push(',');
+            highlighted.push(`|${letters[i]}|`);
+            highlighted.push(',');
+            lastIndex = idx + 1;
+          }
+          return highlighted.join('');
+        }),
+      }));
 
     dispatch(setSearchResultList(resultData.slice(0, 8)));
 
